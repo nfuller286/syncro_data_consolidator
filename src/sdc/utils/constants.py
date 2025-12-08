138edb761b51600b70ffc201a9a8cefbffdd9609
nfuller286 <80130411+nfuller286@users.noreply.github.com>
2025-12-08 14:14:22 -0500

    Added ScreenConnect Local API Gateway. Updated config and ingestor to handle mix of csv or api results. Updated state handling for Screenconnect.

--- src/sdc/utils/constants.py
+++ src/sdc/utils/constants.py
@@ -1,3 +1,24 @@
 from datetime import datetime, timezone
+import uuid
 
 UNDEFINED_TIMESTAMP = datetime(1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+
+# ScreenConnect Constants
+SCREENCONNECT_NAMESPACE_OID = uuid.uuid5(uuid.NAMESPACE_DNS, 'screenconnect.syncromsp.com')
+SCREENCONNECT_DEFAULT_API_LIMIT = 100
+SCREENCONNECT_DEFAULT_FILTER_EXPRESSION = "ConnectionEndUniversal > $TIMESTAMP"
+SCREENCONNECT_QUERY_FIELDS = [
+    "SessionSessionID", 
+    "ProcessType", 
+    "SessionSessionType", 
+    "SessionName", 
+    "ParticipantName",
+    "ConnectedTime", 
+    "DisconnectedTime", 
+    "DurationSeconds", 
+    "ClientType", 
+    "NetworkAddress",
+    "SessionCustomProperty1", 
+    "SessionCustomProperty4", 
+    "SessionCustomProperty5"
+]
