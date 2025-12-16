440a21c1142e1ba9fd80af8a41a5549da91bf580
nfuller286 <nfuller286@gmail.com>
2025-12-16 09:22:02 -0500

    Adjusted Screenconnect ingestor to handle the ScreenConnect API. Set up to pass parameters to the api in order to customize the report.

--- src/sdc/utils/constants.py
+++ src/sdc/utils/constants.py
@@ -6,19 +6,16 @@ UNDEFINED_TIMESTAMP = datetime(1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
 # ScreenConnect Constants
 SCREENCONNECT_NAMESPACE_OID = uuid.uuid5(uuid.NAMESPACE_DNS, 'screenconnect.syncromsp.com')
 SCREENCONNECT_DEFAULT_API_LIMIT = 100
-SCREENCONNECT_DEFAULT_FILTER_EXPRESSION = "ConnectionEndUniversal > $TIMESTAMP"
 SCREENCONNECT_QUERY_FIELDS = [
-    "SessionSessionID", 
-    "ProcessType", 
-    "SessionSessionType", 
-    "SessionName", 
-    "ParticipantName",
-    "ConnectedTime", 
-    "DisconnectedTime", 
-    "DurationSeconds", 
-    "ClientType", 
-    "NetworkAddress",
-    "SessionCustomProperty1", 
-    "SessionCustomProperty4", 
-    "SessionCustomProperty5"
+      "ProcessType",
+      "SessionSessionType",
+      "SessionName",
+      "ParticipantName",
+      "ConnectedTime",
+      "DisconnectedTime",
+      "DurationSeconds",
+      "ConnectionID",
+      "SessionCustomProperty1",
+      "SessionSessionID",
+      "ClientType"
 ]
